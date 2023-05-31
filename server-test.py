7f1d7d3229795a88ebe937e33938f499ee95b78f
aref <armowl@utu.fi>
2023-05-31 11:50:18 +0300

    last

--- server-test.py
+++ server-test.py
@@ -5,11 +5,13 @@ s = socket.socket()
 s.bind(('0.0.0.0', 12355))
 s.listen(50)
 a = list()
+#os.system('touch ~/Desktop/flags.txt')
 def flag():
 	for i in range(10):
 		global a
 		a.append(random.randint(1,10))
 	a = ''.join (str(i) for i in a)
+	os.system(f"echo '{a}' >> ~/Desktop/flags.txt")
 	return a
 while True:
 	c, addr = s.accept()
