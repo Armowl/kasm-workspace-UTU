669bb5882b6d94e7c70d7e2110c842da3cc499a2
aref <armowl@utu.fi>
2023-05-31 11:40:28 +0300

    2

--- client-test.py
+++ client-test.py
@@ -1,6 +1,7 @@
 import socket
 
 s = socket.socket()
-s.connect(('localhost', 12345))
+s.connect(('localhost', 12355))
+print (s.recv(1024))
 s.sendall(b'a')
 s.close()
--- server-test.py
+++ server-test.py
@@ -1,12 +1,21 @@
 import socket
-
+import os
+import random
 s = socket.socket()
-s.bind(('0.0.0.0', 12345))
-
-s.listen(20)
+s.bind(('0.0.0.0', 12355))
+s.listen(50)
+a = list()
+def flag():
+	for i in range(10):
+		global a
+		a.append(random.randint(1,10))
+	a = ''.join (str(i) for i in a)
+	return a
 while True:
 	c, addr = s.accept()
 	print ('got connection from', addr)
+	c.sendall(flag().encode())
 	print (c.recv(1024))
 	c.close()
+	
 
